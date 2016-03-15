158e524b77ebeef2deb6db6278fdc5cca791011e
Nemanja Tomic <nemanja@tkn.rs>
2016-03-15 11:23:28 +0100

    Created RabbitLogger implementation of IQueueLogger.

---
+++ src/Tkn.Queuer.RabbitMQ/Logger/LoggerConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Tkn.Queuer.RabbitMQ.Logger {
+	internal static class LoggerConstants {
+		internal const string INFO = "INFO";
+		internal const string ERROR = "ERROR";
+		internal const string DEBUG = "DEBUG";
+		internal const string EXCHANGE_NAME = "LOG";
+		internal const string VIRTUAL_HOST = "Common";
+	}
+}
---
+++ src/Tkn.Queuer.RabbitMQ/Logger/RabbitLogger.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using Tkn.Queuer.Interface;
+using Tkn.Queuer.Models;
+
+namespace Tkn.Queuer.RabbitMQ.Logger {
+	public class RabbitLogger : IQueueLogger {
+		readonly QueueSettingsModel _queueSettings;
+		IConnection _connection;
+		ConnectionFactory _connectionFactory;
+		IModel _model;
+
+		#region Constructors
+
+		public RabbitLogger(QueueSettingsModel settings) {
+			_queueSettings = settings;
+
+			initializeRabbit();
+		}
+
+		#endregion
+
+		#region Interface implementation
+
+		public void Info(string message) {
+			send(LoggerConstants.INFO, message);
+		}
+
+		public void Error(string message) {
+			send(LoggerConstants.ERROR, message);
+		}
+
+		#endregion
+
+		void initializeRabbit() {
+			_connectionFactory = new ConnectionFactory {
+				HostName = _queueSettings.Hostname,
+				UserName = _queueSettings.Username,
+				Password = _queueSettings.Password,
+				VirtualHost = LoggerConstants.VIRTUAL_HOST
+			};
+			if (_queueSettings.Port > 0)
+				_connectionFactory.Port = _queueSettings.Port;
+
+			_connection = _connectionFactory.CreateConnection();
+			_model = _connection.CreateModel();
+		}
+
+		void send(string level, string message) {
+			var properties = _model.CreateBasicProperties();
+			properties.Persistent = true;
+
+			var messageBuffer = Encoding.Default.GetBytes(message);
+			_model.BasicPublish(LoggerConstants.EXCHANGE_NAME, $"{_queueSettings.ApplicationName}.{level}", properties, messageBuffer);
+		}
+	}
+}
